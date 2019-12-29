8f27afa9af92e2132ea6b1bb00c17c8422765ec4
dAtun <denizatun@gmail.com>
2019-12-29 20:36:19 +0100

    Added explanations and fixes
    - Changed JacobA and JacobG:
    They are useless, explained in comments.
    
    - Due to the changes to JacA and JacG, commented zero parts from gen_p_minus. ( aka eq(4) )
    
    - Tried to make some functions understandable for Sid

--- lab3-ExtendedKalmanFilter.py
+++ lab3-ExtendedKalmanFilter.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import symbols, diff
 
 from addon.generate_data_2D import gen_2D_data as gen2D
 
+from sympy import symbols, diff
 # Sympy also has jacobi stuff, but it may be for polynomials
 # I may remove it if we don't ever use diff stuff
 
+
 class ExtendedKF:
     def __init__(self, T_in):
-
 
         self.matR = None
 
@@ -21,7 +21,7 @@ class ExtendedKF:
         self.X_pred_list = []
         self.X_corr_list = []
 
-        self.z = np.zeros((1,4))
+        self.z = np.zeros((1, 4))
 
         # matQ by Eq(10)
         self.matQ = np.array([[self.Ti**3/3, self.Ti**2/2, 0, 0],
@@ -29,10 +29,19 @@ class ExtendedKF:
                                 [0, 0, self.Ti**3/3, self.Ti**2/2],
                                 [0, 0, self.Ti**2/2, self.Ti**3/3]])
         # matA or part of f by Eq(10)
-        self.f_KF = np.array([[1, self.Ti, 0, 0],
-                                [0, 1 ,0 ,0],
+        self.matA = np.array([[1, self.Ti, 0, 0],
+                                [0, 1, 0, 0],
                                 [0, 0, 1, self.Ti],
                                 [0, 0, 0, 1]])
+
+    def x_hat_0(self, z_in):
+        z_0 = z_in[0]
+        z_1 = z_in[1]
+        x_1 = z_0[0]
+        y_1 = z_0[2]
+        v_x = z_1[0] - z_0[0] / self.Ti
+        v_y = z_1[2] - z_0[2] / self.Ti
+        return np.array([x_1, v_x, y_1, v_y]).reshape((1,4))
 
     def h_KF(self,x_k):
 
@@ -49,7 +58,6 @@ class ExtendedKF:
         Converting velocity values to polar as r_v and theta_v
         """
 
-
         r_x = np.sqrt(x_k[0]**2 + x_k[2]**2)
         theta_x = np.arctan(x_k[0]/x_k[2]) * 180 / np.pi
         r_v = np.sqrt(x_k[1]**2 + x_k[3]**2)
@@ -64,73 +72,73 @@ class ExtendedKF:
             self.z = np.vstack((self.z, self.h_KF(x_in[i])))
         self.z = np.delete(self.z, 0, 0)
 
-
-    def process_noise(self, size, Q):
-        # Ex: process_noise(self, (1,3), Q)
-        self.matQ = np.random.normal(0, Q, size)
-
     def meas_noise(self, size, R):
         # Ex: meas_noise(self, (1,3), Q)
         self.matR = np.random.normal(0, R, size)
 
-    def jacobA(self,T):
-    # Calculates the Jacobian A at every time step.
-    # Needs the time step T as an input
-        A = np.array([
-                        [1, T, 0, 0],
-                        [0, 1, 0, 0],
-                        [0, 0, 1, T],
-                        [0, 0, 0, 1]
-                    ])
-        return A
-
-    def jacobG(self,k):
+    def jacobA(self):
+        print("Read commented section!")
+        print("If we will have additional time after documenting, we will support comment explanation with code!")
+        # This was intended for Jacobian matrix of part. der. of f w.r.t. state x.
+        # Considering the equation x(k) = matA @ x(k-1) + █ * u(k) + █ * 0
+        # derivative wrt state x yields matrix A.
+        # Thus A[i,j] Jacobian is always A
         return
+
+    def jacobG(self):
+        print("Read commented section!")
+        print("If we will have have additional time after documenting, we will support comment explanation with code!")
+        # This was intended for Jacobian matrix of part. der. of f w.r.t. process noise w(k).
+        # Considering the equation x(k) = matA @ x(k-1) + █ * u(k) + █ * 0 (where 0 = w(k))
+        # As given in the pdf, w(k) is equal to zero, which is a constant. Derivative of constant also yields 0.
+        # Thus G[i,j] Jacobian is always 0.
+        return
+
     def jacobC(self,x_k):
         return
     def jacobH(self,x_k):
         return
-    def t_up_x_pred(self,x_corr):
+
+    def gen_x_hat_minus(self, x_corr_k, w_k):
         # Eq (3)
-        self.X_pred_list.append(self.f_KF @ x_corr + self.matQ)
+        self.X_pred_list.append(self.matA @ x_corr_k + w_k)
 
-    def t_up_p_pred(self, P_corr,Q_k,k):
+    def gen_p_minus(self, P_corr):
         # Eq(4)
-        matA = self.jacobA(k)
-        matG = self.jacobG(k)
-        self.P_pred_list.append(matA @ P_corr @ matA.T + matG @ Q_k @ matG.T)
+        self.P_pred_list.append(self.matA @ P_corr @ self.matA.T)  # + matG @ Q_k @ matG.T) commented due to derivative
 
-    def m_up_k_gain(self, P_pred_k, step):
+    def gen_k_gain(self, P_pred_k, step):
         # Eq(5)
         temp1 = np.linalg.inv(self.jacobC(step) @ P_pred_k @ self.jacobC(step).T +
                               self.jacobH(step) @ self.matR @ self.jacobH(step).T)
         self.K_gain_list.append(P_pred_k @ self.jacobC(step).T @ temp1)
 
-    def m_up_x_corr(self,X_pred_k, K_k, Z_k):
+    def corr_x_hat(self, X_pred_k, K_k, Z_k):
         # Eq(6)
         self.X_corr_list.append(X_pred_k + K_k @ (Z_k - self.h_KF(X_pred_k)))
 
-    def m_up_p_corr(self,K_k, P_pred_k, step):
+    def corr_p(self, K_k, P_pred_k, step):
         # Eq(7)
         # identity 5 to be changed to a matrix size stuff
         temp1 = np.linalg.inv(P_pred_k)
         self.P_corr_list.append((np.identity(5) - K_k @ self.jacobC(step)) @ temp1)
 
     def KalmanFiltering(self):
+        w = np.random.normal(0, self.matQ, 4)
         for i, meas in enumerate(self.z):
-            self.t_up_x_pred(self.X_corr_list[i])
-            self.t_up_p_pred(self.P_corr_list[i], self.matQ[i], i)  # matrix Q at step k? wtf is that
-            self.m_up_k_gain(self.P_pred_list[i],i)
-            self.m_up_x_corr(self.X_pred_list[i],self.K_gain_list[i],meas)
-            self.m_up_p_corr(self.K_gain_list[i],self.P_pred_list[i],i)
+            self.gen_x_hat_minus(self.X_corr_list[i], w[i])
+            self.gen_p_minus(self.P_corr_list[i])
+            self.gen_k_gain(self.P_pred_list[i], i)
+            self.corr_x_hat(self.X_pred_list[i], self.K_gain_list[i], meas)
+            self.corr_p(self.K_gain_list[i], self.P_pred_list[i], i)
 
 
-def nees(x_true,x_pred,p_list):
+def nees(x_true, x_pred, p_list):
     x_inter = x_true - x_pred
     return x_inter @ np.linalg.inv(p_list) @ x_inter.T
 
 
-def nis(x_pred,z_in,p_list,C_in,H_in,R_in):
+def nis(x_pred, z_in, p_list, C_in, H_in, R_in):
     S_k = C_in @ p_list @ C_in.T + H_in @ R_in @ H_in.T
     z1_k = z_in - C_in @ x_pred.T
     return z1_k.T @ np.linalg.inv(S_k) @ z1_k
