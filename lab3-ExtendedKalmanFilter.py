Jacobian of C and small fixes
## Changes
-> Small indentation fixes
-> Changed matrix size of z from (1,4) to (1,2)
-> Changed jacobG from identity matrix to zero matrix (doesn't matter that much actually)
-> Added the hand-calculated result of Jacobian C as hardJacobC
-> Modified rest of the functions to use jacobC

## Important Notes
[ ! ] Currently, jacobC is calculated out of the class. If we want to store values, we can place it inside as a function and define variable
[ ! ] Due to the given equations and initial values, w(k) process noise is being nullified.
x_minus_k = f( x_hat(k-1), u(k-1),  0);
where 0 = w(k)

lab3-ExtendedKalmanFilter.py
@@ -10,7 +10,7 @@ from sympy import symbols, diff
 
 class ExtendedKF:
     def __init__(self, T_in, sigma):
-    # Initialise piecewise contant velocity model.
+        # Initialise piecewise constant velocity model.
 
         self.matR = None
         self.Ti = T_in
@@ -19,7 +19,7 @@ class ExtendedKF:
         self.K_gain_list = []
         self.x_hat_minus = []
         self.x_hat = []
-        self.z = np.zeros((1, 4))
+        self.z = np.zeros((1, 2))
 
         # # matQ by Eq(10)
         # self.matQ = np.array([[self.Ti**3/3, self.Ti**2/2, 0, 0],
@@ -38,9 +38,8 @@ class ExtendedKF:
         # matrix G relates the process noise w to the state x
         self.matG = np.identity(4)
 
-    # matrix G relates the process noise w to the state x
-        self.matH = np.identity(2)
-
+        # matrix G relates the process noise w to the state x
+        self.matH = np.zeros((2,2))
 
     def get_x_hat_0(self, z_in):
         z_0 = z_in[0]
@@ -56,7 +55,7 @@ class ExtendedKF:
         y = z0_y
         v_x = z1_x - z0_x / self.Ti
         v_y = z1_y - z1_y / self.Ti
-        return np.array([x, v_x, y, v_y]).reshape((1,4))
+        return np.array([x, v_x, y, v_y]).reshape((1, 4))
 
     def get_P_0(self):
         P_0 = np.identity(4)
@@ -100,8 +99,7 @@ class ExtendedKF:
         # r_v = (x_k[0] * x_k[1] + x_k[2] * x_k[3]) / np.sqrt(x_k[0]**2 + x_k[2]**2)
         # theta_v = (x_k[0] * x_k[3] - x_k[1] * x_k[2]) / (x_k[0]**2 + x_k[2]**2)
 
-
-        return np.array([r_xy, theta_xy, r_v, theta_v]).reshape(1, 4)
+        return np.array([r_xy, theta_xy]).reshape(1, 2)
 
     def gen_z(self, x_in):
         for i in range(len(x_in)):
@@ -138,8 +136,15 @@ class ExtendedKF:
         # /// https://mathworks.com/help/driving/ug/extended-kalman-filters.html
         return
 
-    def jacobC(self):
-        return
+    def hardJacobC(self, x_in):
+        # The partial derivative of function h is calculated on paper and provided in this function to only compute
+        # given input data (aka hardcoded the Jacobian matrix). Depending on the time we have left, we may also add
+        # dynamic variant where it calculates wrt to the input vector.
+        # x_in => x_minus_k
+        jac_c = np.array([[-x_in[0] / (x_in[0]**2 + x_in[2]**2),       1/x_in[0]*(1+(x_in[2]/x_in[0])**2),       0, 0],
+                          [x_in[0] / np.sqrt(x_in[0]**2 + x_in[2]**2), x_in[2]/np.sqrt(x_in[0]**2 + x_in[2]**2), 0, 0]
+                          ])
+        return jac_c.reshape(2, 4)
 
     def jacobH(self):
         print("Read commented section!")
@@ -161,20 +166,20 @@ class ExtendedKF:
         # Eq(4)
         self.P_pred_list.append(self.matA @ P_corr @ self.matA.T)  # + matG @ Q_k @ matG.T) commented due to derivative
 
-    def gen_k_gain(self, P_pred_k, step):
+    def gen_k_gain(self, P_pred_k, jacobC):
         # Eq(5)
-        temp1 = np.linalg.inv(self.jacobC(step) @ P_pred_k @ self.jacobC(step).T + self.matR)
-        self.K_gain_list.append(P_pred_k @ self.jacobC(step).T @ temp1)
+        temp1 = np.linalg.inv(jacobC @ P_pred_k @ jacobC.T + self.matR)
+        self.K_gain_list.append(P_pred_k @ jacobC.T @ temp1)
 
     def corr_x_hat(self, X_pred_k, K_k, Z_k):
         # Eq(6)
         self.x_hat.append(X_pred_k + K_k @ (Z_k - self.h_KF(X_pred_k)))
 
-    def corr_p(self, K_k, P_pred_k, step):
+    def corr_p(self, K_k, P_pred_k, jacobC):
         # Eq(7)
         # identity 5 to be changed to a matrix size stuff
         temp1 = np.linalg.inv(P_pred_k)
-        self.P_corr_list.append((np.identity(5) - K_k @ self.jacobC(step)) @ temp1)
+        self.P_corr_list.append((np.identity(5) - K_k @ jacobC) @ temp1)
 
     def KalmanFiltering(self):
 
@@ -182,11 +187,12 @@ class ExtendedKF:
         # Q in this case is the process noise variance.
         w = np.random.normal(0, self.matQ, 4)
         for i, meas in enumerate(self.z):
-            self.gen_x_hat_minus(self.x_hat[i], w[i])
+            self.gen_x_hat_minus(self.x_hat[i])
             self.gen_p_minus(self.P_corr_list[i])
-            self.gen_k_gain(self.P_pred_list[i], i)
+            matC_k = self.hardJacobC(self.x_hat_minus[i])
+            self.gen_k_gain(self.P_pred_list[i], matC_k)
             self.corr_x_hat(self.x_hat_minus[i], self.K_gain_list[i], meas)
-            self.corr_p(self.K_gain_list[i], self.P_pred_list[i], i)
+            self.corr_p(self.K_gain_list[i], self.P_pred_list[i], matC_k)
 
 
 def nees(x_true, x_pred, p_list):
@@ -201,7 +207,7 @@ def nis(x_pred, z_in, p_list, C_in, H_in, R_in):
 
 
 def main():
-    gen_data = gen2D(10,10,10,1e-3)  # init real values, measured values etc.
+    gen_data = gen2D(10, 10, 10, 1e-3)  # init real values, measured values etc.
     extKF_T = ExtendedKF(5)  # T value for initialising
     extKF_T.gen_z(gen_data.x)
     print(7)